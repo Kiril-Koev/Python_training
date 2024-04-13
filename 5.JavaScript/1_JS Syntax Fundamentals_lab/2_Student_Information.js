3dccea17931991b6cc0cebcda7d640c3c6edf1c1
Kiril-Koev <122820032+Kiril-Koev@users.noreply.github.com>
2024-04-13 13:02:22 +0300

    Folder name change

--- 5.JavaScript/1_JS Syntax Fundamentals_lab/2_Student_Information.js
+++
@@ -1,5 +0,0 @@
-function solve(name,age,grade){
-    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
-}
-
-// solve("Ivan",2,8);
--- 5.JavaScript/1_JS Syntax Fundamentals_lab/9_Numbers_From_1_to_10.js
+++
@@ -1,7 +0,0 @@
-function NumbersOneToFive(){
-    for (let i=1; i <= 5; i++){
-        console.log(i)
-    }
-}
-
-// NumbersOneToFive()
